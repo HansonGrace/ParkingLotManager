21edba42a53f652e4acec0d55d3d00e3da975d7a
Grace Hanson <hansongrace2004@gmail.com>
2025-06-03 21:21:33 -0400

    declare first classes

---
+++ src/ParkingLot.java
@@ -0,0 +1,17 @@
+
+import java.util.List;
+
+public class ParkingLot {
+	
+	private String ParkingLotId;
+	private List<List<Slot>> slots;
+	
+	  public ParkingLot(String parkingLotId, List<List<Slot>> slots) {
+	       
+	    }
+	  
+	  public String parkVehicle(String type, String regNo, String color) {
+		return color;
+		  
+	  }
+}
---
+++ src/Slot.java
@@ -0,0 +1,17 @@
+
+
+public class Slot {
+	
+	
+	private String type;
+	private Vehicle vehicle;
+	private String ticketId;
+	
+	public Slot(String type) {
+		this.type = type;
+		this.vehicle = null;
+		this.ticketId = null;
+	}	
+	
+
+}
